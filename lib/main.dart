260b94705c6e80727a0267a8aad7364f1b6b0d48
Congminhdeptrainhatnha <minh.vcm1999@gmail.com>
2023-10-24 21:57:01 +0700

    Merge branch 'main' of https://github.com/vocongminh0909/nhom4_appraovat

--- lib/main.dart
+++ lib/main.dart
@@ -100,8 +100,8 @@ class _HomePageState extends State<HomePage> {
                           ),
                           onPressed: () {
                              Navigator.push(context, MaterialPageRoute(builder: (context) => SearchProductPage()));
- },
-),
+                             },
+                             ),
                     ),
                   ),
                 ),
